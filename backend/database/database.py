e11011783fe2647c55fb86bfa4e91d0c5d04d4dd
Ryan <ryanchristiancastillo0@gmail.com>
2026-07-11 09:34:51 +0800

    updated env deployment sa database

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,9 +1,24 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import Session
-DATABASE_URL = "mysql+pymysql://root:@localhost/chicken_ai"
 
-engine = create_engine(DATABASE_URL)
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_NAME = os.environ["DB_NAME"]
+DB_SSL_CA = os.environ["DB_SSL_CA"]
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "ca": DB_SSL_CA
+        }
+    }
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -12,14 +27,9 @@ SessionLocal = sessionmaker(
 )
 
 
-
-
 def get_db():
-
     db = SessionLocal()
-
     try:
         yield db
-
     finally:
         db.close()
